44902a644ac417183f59db7d5f8e85c0e69c2c08
hyperbola <me@hyperbola.me>
2023-07-09 16:25:41 +0800

    feat: Update no-shadow rule for TypeScript

--- typescript.js
+++ typescript.js
@@ -1,12 +1,18 @@
+const conflictedJavascriptRules = {
+  // JavaScript no-shadow rule doesn't work with TypeScript.
+  'no-shadow': 'off'
+}
+
 const typescriptRules = {
-  '@typescript-eslint/no-implicit-any-catch': 'warn'
+  '@typescript-eslint/no-implicit-any-catch': 'warn',
+  '@typescript-eslint/no-shadow': 'error'
 }
 
 module.exports = {
   extends: ['plugin:@typescript-eslint/recommended', 'plugin:import/typescript'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  rules: typescriptRules,
+  rules: Object.assign(conflictedJavascriptRules, typescriptRules),
   settings: {
     // https://github.com/import-js/eslint-plugin-import/tree/main#typescript
     'import/resolver': {
