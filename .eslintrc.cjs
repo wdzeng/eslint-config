44fb2e6e85e4fbdd45e25bf3b41292da0cc6a3de
hyperbola <me@hyperbola.me>
2023-07-15 04:33:22 +0800

    clean: remove redundant tsconfig.json and shrink .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,8 @@
 /* eslint-env node */
 
-const { rules } = require('./index.js')
-const { rules: tsRules } = require('./typescript.js')
-
-const overriddenRules = {
-  'unicorn/prefer-module': 'off',
-  'prettier/prettier': 'warn'
-}
-
 module.exports = {
   root: true,
-  extends: ['eslint:recommended', 'plugin:import/recommended', 'plugin:prettier/recommended'],
-  plugins: ['unicorn', 'import', 'prettier'],
+  extends: ['./index'],
   env: {
     browser: false,
     es2022: true,
@@ -22,29 +13,8 @@ module.exports = {
     sourceType: 'module',
     project: './tsconfig.json'
   },
-  rules: Object.assign({}, rules, overriddenRules),
-
-  overrides: [
-    {
-      files: ['*.ts'],
-      extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/strict-type-checked',
-        'plugin:@typescript-eslint/stylistic-type-checked',
-        'plugin:import/recommended',
-        'plugin:import/typescript',
-        'plugin:prettier/recommended'
-      ],
-      parser: '@typescript-eslint/parser',
-      plugins: ['unicorn', 'import', '@typescript-eslint', 'prettier'],
-      rules: Object.assign({}, tsRules, overriddenRules),
-      settings: {
-        // https://github.com/import-js/eslint-plugin-import/tree/main#typescript
-        'import/resolver': {
-          node: true,
-          typescript: true
-        }
-      }
-    }
-  ]
+  rules: {
+    'unicorn/prefer-module': 'off',
+    'prettier/prettier': 'warn'
+  }
 }
