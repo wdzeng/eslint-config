57bf4fdd6baca0f87b285d6b55214e94aa09822b
hyperbola <me@hyperbola.me>
2023-07-14 19:57:41 +0800

    fix: typescript preset does not extends regular preset

--- typescript.js
+++ typescript.js
@@ -1,14 +1,24 @@
+const importOverrides = require('./presets/import-overrides.js')
+const recommendedOverrides = require('./presets/recommended-overrides.js')
 const typescriptOverrides = require('./presets/typescript-overrides.js')
+const unicornSelections = require('./presets/unicorn-selections.js')
 
 module.exports = {
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/strict-type-checked',
     'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:import/recommended',
     'plugin:import/typescript'
   ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  rules: Object.assign(typescriptOverrides),
+  plugins: ['unicorn', 'import', '@typescript-eslint'],
+  rules: Object.assign(
+    recommendedOverrides,
+    typescriptOverrides,
+    unicornSelections,
+    importOverrides
+  ),
   settings: {
     // https://github.com/import-js/eslint-plugin-import/tree/main#typescript
     'import/resolver': {
