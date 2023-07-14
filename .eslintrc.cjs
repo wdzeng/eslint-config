45d64f779a90de5f4e2311cc07ec57e11791ad4b
hyperbola <me@hyperbola.me>
2023-07-14 22:25:38 +0800

    feat: update eslintrc after presets are updated and support typescript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,7 @@
 /* eslint-env node */
 
 const { rules } = require('./index.js')
+const { rules: tsRules } = require('./typescript.js')
 
 const overriddenRules = {
   'unicorn/prefer-module': 'off',
@@ -9,8 +10,8 @@ const overriddenRules = {
 
 module.exports = {
   root: true,
-  extends: ['plugin:prettier/recommended'],
-  plugins: ['unicorn', 'import'],
+  extends: ['eslint:recommended', 'plugin:import/recommended', 'plugin:prettier/recommended'],
+  plugins: ['unicorn', 'import', 'prettier'],
   env: {
     browser: false,
     es2022: true,
@@ -18,7 +19,32 @@ module.exports = {
   },
   parserOptions: {
     ecmaVersion: 13,
-    sourceType: 'commonjs'
+    sourceType: 'commonjs',
+    project: './tsconfig.json'
   },
   rules: Object.assign({}, rules, overriddenRules),
+
+  overrides: [
+    {
+      files: ['*.ts'],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/strict-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked',
+        'plugin:import/recommended',
+        'plugin:import/typescript',
+        'plugin:prettier/recommended'
+      ],
+      parser: '@typescript-eslint/parser',
+      plugins: ['unicorn', 'import', '@typescript-eslint', 'prettier'],
+      rules: Object.assign({}, tsRules, overriddenRules),
+      settings: {
+        // https://github.com/import-js/eslint-plugin-import/tree/main#typescript
+        'import/resolver': {
+          node: true,
+          typescript: true
+        }
+      }
+    }
+  ]
 }
