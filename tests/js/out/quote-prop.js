d33e5a51770cdeb0cae4f34b2e7c745e076f2ab8
hyperbola <me@hyperbola.me>
2023-07-22 04:05:59 +0800

    test: add testcases for `quote-props`

---
+++ tests/js/out/quote-prop.js
@@ -0,0 +1,5 @@
+export const noQuotesAreGood = { a: 'a', b: 'b' }
+
+export const theseQuotesAreRedundant = { a: 'a', b: 'b' }
+
+export const quotesShouldBeConsistent = { 'a': 'a', 'i-need-quotes': 'i-need-quotes' }
