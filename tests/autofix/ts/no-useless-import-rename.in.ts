3d6e57e4133abd480adf5c3657cf56f40768f99d
hyperbola <me@hyperbola.me>
2025-03-25 01:55:49 +0800

    chore: fix typo in a auto-fixing test

--- tests/autofix/ts/no-useless-import-rename.in.ts
+++ tests/autofix/ts/no-useless-import-rename.in.ts
@@ -1,3 +1,3 @@
 // Please also update JS/TS test if you are changing this file.
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { join as join } from 'path'
+import { join as join } from 'node:path'
