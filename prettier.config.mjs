b1ee4d8bad5c7c961cbc3fc0e76ddeacd7bb0443
hyperbola <me@hyperbola.me>
2025-03-27 18:49:19 +0800

    chore: update prettier and vitest configuration file extension name
    Use .config.mjs.

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+export default {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  quoteProps: 'consistent',
+  endOfLine: 'lf'
+}
